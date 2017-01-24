1e318ed6b5f00406733d8756be153f9797eabbc0
Félix Boulet <felix.boulet@polymtl.ca>
2017-01-24 11:13:14 -0500

    Modification du petit script pour installer automatiquement les 6 npm install. On peut maintenant spécifier les arguments --dry-run (pour seulement vérifier sans installer) et/ou --silent (pour réduire la pollution du terminal) lors de l'exécution du script. Le script vérifie également si la structure des dossiers est correcte avant de s'exécuter.

--- npminstall.sh
+++ npminstall.sh
@@ -1,27 +1,53 @@
-!#/bin/bash
-cd Sudoku/server
-echo "Installing Sudoku server modules..."
-npm install
-echo "Sudoku server modules installed."
-cd ../client
-echo "Installing Sudoku client modules..."
-npm install
-echo "Sudoku client modules installed."
-cd ../../Scrabble/server
-echo "Installing Scrabble server modules..."
-npm install
-echo "Scrabble server modules installed."
-cd ../client
-echo "Installing Scrabble client modules..."
-npm install
-echo "Scrabble client modules installed."
-cd ../../Curling/server
-echo "Installing Curling server modules..."
-npm install
-echo "Curling server modules installed."
-cd ../client
-echo "Installing Curling client modules..."
-npm install
-echo "Curling client modules installed."
-cd ..
-echo "NPM Install done!"
+#!/bin/bash
+SUS="Sudoku/server"
+SUC="Sudoku/client"
+SCS="Scrabble/server"
+SCC="Scrabble/client"
+CUS="Curling/server"
+CUC="Curling/client"
+MOD=""
+
+if [ "$1" = "--dry-run" -o "$1" = "--silent" ]
+then
+  MOD="$1"
+  echo "Running script with option "$1
+  if [ "$2" = "--dry-run" -o "$2" = "--silent" ]
+  then
+    MOD="$1 $2"
+    echo "Running script with 2nd option "$2
+  fi
+fi
+
+echo "Checking if directory structure is good..."
+if [ -d $SUS -a -d $SUC -a -d $SCS -a -d $SCC -a -d $CUS -a -d $CUC ]
+then
+  echo "Directory structure is good, beginning npm install task..."
+  cd Sudoku/server
+  echo "Installing Sudoku server modules..."
+  npm install $MOD
+  echo "Sudoku server modules installed."
+  cd ../client
+  echo "Installing Sudoku client modules..."
+  npm install $MOD
+  echo "Sudoku client modules installed."
+  cd ../../Scrabble/server
+  echo "Installing Scrabble server modules..."
+  npm install $MOD
+  echo "Scrabble server modules installed."
+  cd ../client
+  echo "Installing Scrabble client modules..."
+  npm install $MOD
+  echo "Scrabble client modules installed."
+  cd ../../Curling/server
+  echo "Installing Curling server modules..."
+  npm install $MOD
+  echo "Curling server modules installed."
+  cd ../client
+  echo "Installing Curling client modules..."
+  npm install $MOD
+  echo "Curling client modules installed."
+  cd ..
+  echo "NPM Install done!"
+else
+  echo "Directory structure is wrong! Check if all directories are at the right place."
+fi
