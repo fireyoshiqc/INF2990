e31f42c3d94af3221d48fb3846ff9f2dfa222c0f
Mikael Ferland <mikael.ferland@gmail.com>
2017-01-27 13:44:21 -0500

    Affichage de la grille de sudoku statique côté client.

--- Sudoku/client/app/app.component.ts
+++ Sudoku/client/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>Sudoku</h1>
+    <sudokuGrid></sudokuGrid>
+  `,
 })
-export class AppComponent { name = 'Angular'; }
+export class AppComponent { }
---
+++ Sudoku/client/app/components/sudokuGrid.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sudoku-grid',
+    template: `
+    <table>
+        <tr>
+            <td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td>
+        </tr>
+        <tr>
+            <td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td>
+        </tr>
+        <tr>
+            <td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td>
+        </tr>
+        <tr>
+            <td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td>
+        </tr>
+        <tr>
+            <td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td>
+        </tr>
+        <tr>
+            <td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td>
+        </tr>
+        <tr>
+            <td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td>
+        </tr>
+        <tr>
+            <td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td>
+        </tr>
+        <tr>
+            <td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td><td>x</td>
+        </tr>
+    </table>
+    `,
+})
+export class SudokuGridComponent { sudokuGrid = 'test'; }
