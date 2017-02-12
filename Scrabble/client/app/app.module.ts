593e1fca4377c2671eb909c8efc7b6f2a77bd43f
Pierre To <pierre.to@polymtl.ca>
2017-02-11 21:40:51 -0500

    En lien avec le dernier commit (oublier un fichier)

--- Scrabble/client/app/app.module.ts
+++ Scrabble/client/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './components/app.component';
 import { ChatComponent } from './components/chat.component';
@@ -8,7 +9,7 @@ import { BoardComponent } from './components/board.component';
 import { RackComponent } from './components/rack.component';
 
 @NgModule({
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule, FormsModule, MaterialModule.forRoot()],
     declarations: [AppComponent, ChatComponent, BoardComponent, RackComponent],
     bootstrap: [AppComponent]
 })
