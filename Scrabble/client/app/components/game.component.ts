3f4920571591bcc3dae47a129ddd43a87d9e466a
Erica Budgen <erica.bugden@gmail.com>
2017-02-15 12:49:35 -0500

    Changed file name for game component html

--- Scrabble/client/app/components/game.component.ts
+++ Scrabble/client/app/components/game.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'game-comp',
-    templateUrl: '/assets/templates/app.component.html'
+    templateUrl: '/assets/templates/game.component.html'
 })
 export class GameComponent { name = 'Angular'; }
