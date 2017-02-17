71d01e8ade1dce112cdb20840c6eea2a74ff9346
Mikael Ferland <mikael.ferland@gmail.com>
2017-02-17 11:55:29 -0500

    Modify cadriciel for sudoku server.

--- Sudoku/server/systemjs.config.js
+++ Sudoku/server/systemjs.config.js
@@ -11,7 +11,7 @@
     // map tells the System loader where to look for things
     map: {
       // other libraries
-      app: 'out/'
+      app: 'out'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
