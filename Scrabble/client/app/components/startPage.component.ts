59c9987882d98bff397c1798892e63c3583d652b
Yawen <yawen.hou@polymtl.ca>
2017-02-17 17:40:29 -0500

    Fixed merged for startPage.component.ts

--- Scrabble/client/app/components/startPage.component.ts
+++ Scrabble/client/app/components/startPage.component.ts
@@ -1,21 +1,32 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { PlayerManagerService } from '../services/playerManager.service';
 
 @Component({
-    selector: 'startPage-comp',
-    templateUrl: '/assets/templates/startPage.component.html'
+    selector: 'startpage-comp',
+    templateUrl: '/assets/templates/startPage.component.html',
+    providers:[PlayerManagerService]
 })
 export class StartPageComponent {
-    nbPlayers: number;
+    capacity: number;
     playerName: string;
 
-    constructor(public router: Router) {
-
+    constructor(public router: Router, private playerManagerService: PlayerManagerService) {
+        this.playerManagerService = playerManagerService;
     }
 
     onJoin() {
-        console.log(this.nbPlayers, this.playerName);
+        console.log(this.capacity, this.playerName);
+        this.playerManagerService.validateName(this.playerName);
+        if (this.playerManagerService.isNameValid()) {
 
-        this.router.navigate(['/waitingRoom']);
+
+            console.log("name valide!!!");
+            this.playerManagerService.setName(this.playerName);
+            this.playerManagerService.addPlayer();
+
+            this.playerManagerService.setCapacity(this.capacity);
+            this.router.navigate(['/waitingRoom']);
+        }
     }
 }
