56ab47b74c4502c2ef941e4e49ce12632d372525
Mikael Ferland <mikael.ferland@gmail.com>
2017-02-21 16:06:12 -0500

    Move GameAreaComponent html in external file.

--- Sudoku/client/app/components/gameArea.component.ts
+++ Sudoku/client/app/components/gameArea.component.ts
@@ -2,19 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'game-area',
-    template: `
-    <div id="gameArea">
-        <div id="buttonPanel">
-            <button (click)="sudokuGrid.getEasySudoku()">EASY sudoku</button>
-            <button (click)="sudokuGrid.getHardSudoku()">HARD sudoku</button>
-            <button (click)="sudokuGrid.validateSudoku()">Validate sudoku</button>
-            <!--<button (click)="sudokuGrid.resetSudoku()">Reset sudoku</button>-->
-            <button (click)="sudokuGrid.toggleStopwatch()">
-                {{ sudokuGrid.getStopwatchVisibility() ? "Hide" : "Show" }} stopwatch
-            </button>
-        </div>
-        <sudoku-grid #sudokuGrid></sudoku-grid>
-    </div>
-    `
+    templateUrl: '/assets/templates/gameArea.component.html'
 })
 export class GameAreaComponent { }
