56f558d3711d4bd18f41140f46ac561619c079de
Pierre To <pierre.to@polymtl.ca>
2017-02-22 23:34:29 -0500

    Write tests for classes in Scrabble client - BoardTile - Letter

---
+++ Scrabble/client/app/classes/boardTile.spec.ts
@@ -0,0 +1,37 @@
+/**
+ * boardTile.spec.ts
+ *
+ * @authors Pierre To
+ * @date 2017/02/22
+ */
+
+import { BoardTile } from './boardTile';
+import { Letter } from './letter';
+
+import { expect } from 'chai';
+
+describe('BoardTile', () => {
+
+    let boardTile = new BoardTile("Basic");
+
+    describe('Default constructor', () => {
+        it('should construct a BoardTile object.', done => {
+            expect(boardTile).to.not.be.undefined;
+            expect(boardTile).to.be.an.instanceOf(BoardTile);
+            expect(boardTile.getTileType()).to.be.equal("Basic");
+            expect(boardTile.getTexture()).to.be.equal("../../assets/textures/board/Basic.png");
+            expect(boardTile.getIsEmpty()).to.be.true;
+            done();
+        });
+    });
+
+    describe('putLetter()', () => {
+        it('should put a Letter on a BoardTile object.', done => {
+            let letter = new Letter("A", 1);
+            boardTile.putLetter(letter);
+
+            expect(boardTile.getLetter()).to.be.equal(letter);
+            done();
+        });
+    });
+});
---
+++ Scrabble/client/app/classes/letter.spec.ts
@@ -0,0 +1,26 @@
+/**
+ * letter.spec.ts
+ *
+ * @authors Pierre To
+ * @date 2017/02/22
+ */
+
+import { Letter } from './letter';
+
+import { expect } from 'chai';
+
+describe('Letter', () => {
+
+    let letter = new Letter("a", 1);
+
+    describe('Default constructor', () => {
+        it('should construct a BoardTile object.', done => {
+            expect(letter).to.not.be.undefined;
+            expect(letter).to.be.an.instanceOf(Letter);
+            expect(letter.getLetter()).to.be.equal("A");
+            expect(letter.getValue()).to.be.equal(1);
+            expect(letter.getTexture()).to.be.equal("../../assets/textures/letters/A.png");
+            done();
+        });
+    });
+});
