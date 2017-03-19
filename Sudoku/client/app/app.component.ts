c2b1e381363689e6db3ef9df48ec9b139ad5018f
Félix Boulet <fire_yoshi_qc@hotmail.com>
2017-03-19 17:07:58 -0400

    Changed sudoku page title, changed favicon. Saved the old one as a copy.

--- Sudoku/client/app/app.component.ts
+++ Sudoku/client/app/app.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   moduleId: module.id,
   selector: 'my-app',
   templateUrl: '/assets/templates/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+  private readonly PAGE_TITLE = "FACTORY/24 - Sudoku";
+
+  public constructor(private titleService: Title) {
+    this.titleService.setTitle(this.PAGE_TITLE);
+  }
+
+}
