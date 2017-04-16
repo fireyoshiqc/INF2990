3ed6bc4f6357d3476a4719a708aa65ede552e3c0
Pierre To <pierre.to@polymtl.ca>
2017-04-16 12:47:31 -0400

    Embellissement scrabble - Titre de la page web - Icône de la page web

--- Scrabble/client/app/components/app.component.ts
+++ Scrabble/client/app/components/app.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
     selector: 'my-app',
     template: '<main><router-outlet></router-outlet></main>'
 })
-export class AppComponent { }
+export class AppComponent {
+    private readonly PAGE_TITLE = "FACTORY/24 - Scrabble";
+
+    public constructor(private titleService: Title) {
+        this.titleService.setTitle(this.PAGE_TITLE);
+    }
+}
